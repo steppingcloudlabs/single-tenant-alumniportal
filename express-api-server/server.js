b23ba789ffe4813f340c8826aef612dc8bb96c8e
akash9910764902 <prakritidev.4088@gmail.com>
2020-05-11 08:58:22 +0000

    - news CREATE, VIEW APIs
    Change-Id: I42f799ef8ee22cb5b200c8e9e09843c27f6d613e

--- express-api-server/server.js
+++ express-api-server/server.js
@@ -17,9 +17,9 @@ const app = express();
 //logging
 app.use(morgan("dev"));
 app.use(
-  bodyParser.json({
-    limit: "200mb"
-  })
+	bodyParser.json({
+		limit: "200mb"
+	})
 );
 const logging = require("@sap/logging");
 const appContext = logging.createAppContext();
@@ -28,14 +28,16 @@ const helmet = require("helmet");
 // ...
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    styleSrc: ["'self'", "sapui5.hana.ondemand.com"],
-    scriptSrc: ["'self'", "sapui5.hana.ondemand.com"]
-  }
+	directives: {
+		defaultSrc: ["'self'"],
+		styleSrc: ["'self'", "sapui5.hana.ondemand.com"],
+		scriptSrc: ["'self'", "sapui5.hana.ondemand.com"]
+	}
 }));
 // Sets "Referrer-Policy: no-referrer".
-app.use(helmet.referrerPolicy({ policy: "no-referrer" }));
+app.use(helmet.referrerPolicy({
+	policy: "no-referrer"
+}));
 
 // passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
 // 	uaa: {
@@ -55,41 +57,57 @@ var hanaOptions = xsenv.getServices({
 });
 hanaOptions.hana.pooling = true;
 app.use(
-// 	passport.authenticate("JWT", {
-// 		session: false
-// 	}),
+	// 	passport.authenticate("JWT", {
+	// 		session: false
+	// 	}),
 	xsHDBConn.middleware(hanaOptions.hana)
 );
 
 //Compression
 app.use(require("compression")({
-  threshold: "1b"
+	threshold: "1b"
 }));
 // Handling cors
 const cors = require("cors");
 app.use(cors());
 // compress responses
 app.use(compression());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
 });
 
-app.get("/", (req, res) => {
-	res.send("YESS!!");
+app.get("/", async(req, res) => {
+	try {
+		const dbClass = require("sap-hdbext-promisfied")
+		let db = new dbClass(req.db);
+		const statement = await db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
+		const results = await db.statementExecPromisified(statement, [])
+		let result = JSON.stringify({
+			Objects: results
+		})
+		return res.type("application/json").status(200).send(result)
+	} catch (e) {
+		return res.type("text/plain").status(500).send(`ERROR: ${e.toString()}`)
+	}
+});
+//  TESTING STUFF 
+
+app.get("/env", (req, res) => {
+	return res.type("application/json").status(200).send(JSON.stringify(process.env));
 });
 
 // ROUTES
+
 // nef routes
 const nefRoutes = require("./router/nef");
 app.use("/admin/action", nefRoutes);
 
-
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
+	console.log(`Server listening on port: ${port}`);
 });
--- express-api-server/service/nef/index.js
+++ express-api-server/service/nef/index.js
@@ -1,18 +1,62 @@
+const uuid = require("uuid");
 module.exports = () => {
-
-	const viewNews = ({
-		payload
+	
+	const viewnews = ({
+		payload,
+		db
 	}) => {
-		return new Promise((resolve, reject) => {
+		return new Promise(async(resolve, reject) => {
 			try {
-				resolve(payload);
+				// TODO: add pagination using [to, from] clauses in statement. 
+
+				const statement = await db.preparePromisified(
+					`SELECT TOP 1000 "ID","QUESTION","ANSWER","CREATEDAT","CREATEDBY","MODIFIEDAT","MODIFIEDBY" FROM "MULTITENANT_ALUMNIPORTAL_SAP_MULTITENANT_ALUMNIPORTAL_SAP_DB_1"."SCLABS_ALUMNIPORTAL_FAQ_FAQ"`
+				)
+				const results = await db.statementExecPromisified(statement, [])
+				resolve(results);
+
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+	const createnews = ({
+		payload,
+		db
+	}) => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				const statement = await db.preparePromisified(
+					`INSERT INTO "MULTITENANT_ALUMNIPORTAL_SAP_MULTITENANT_ALUMNIPORTAL_SAP_DB_1"."SCLABS_ALUMNIPORTAL_FAQ_FAQ" VALUES('${payload.ID}', '${payload.QUESTION}', '${payload.ANSWER}', '${payload.CREATEDAT}', '${payload.CREATEDBY}', '${payload.MODIFIEDAT}','${payload.MODIFIEDBY}')`
+				)
+				const results = await db.statementExecPromisified(statement, [])
+				resolve(results);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	};
+
+	const updatenews = ({
+		payload,
+		db
+	}) => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				console.log(
+					`INSERT INTO "MULTITENANT_ALUMNIPORTAL_SAP_MULTITENANT_ALUMNIPORTAL_SAP_DB_1"."SCLABS_ALUMNIPORTAL_FAQ_FAQ" VALUES('${payload.ID}', '${payload.QUESTION}', '${payload.ANSWER}', '${payload.CREATEDAT}', '${payload.CREATEDBY}', '${payload.MODIFIEDAT}','${payload.MODIFIEDBY}')`
+				)
+				resolve(payload)
 			} catch (error) {
 				reject(error);
 			}
 		});
 	};
 	return {
-		viewNews
+		viewNews,
+		createnews,
+		updatenews
 	};
 
 };
