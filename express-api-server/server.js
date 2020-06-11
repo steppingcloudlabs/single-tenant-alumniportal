fc80c1dcfd5443ea3eeb532d7f81c2fafbf90e7a
prakritidev <prakritidev.4088@gmail.com>
2020-06-11 10:41:15 +0000

    added user and admin routes, services, controller files.

--- express-api-server/server.js
+++ express-api-server/server.js
@@ -1,133 +1,146 @@
- /*eslint no-console: 0*/
- "use strict";
+  /*eslint no-console: 0*/
+  "use strict";
+  const express = require("express");
+  const morgan = require("morgan");
+  const bodyParser = require("body-parser");
+  const compression = require("compression");
+  const xsenv = require("@sap/xsenv");
+  const xssec = require("@sap/xssec");
+  const xsHDBConn = require("@sap/hdbext");
 
- const express = require("express");
- const morgan = require("morgan");
- const bodyParser = require("body-parser");
- const compression = require("compression");
- const xsenv = require("@sap/xsenv");
- const xssec = require("@sap/xssec");
- const xsHDBConn = require("@sap/hdbext");
+  const passport = require("passport");
+  const port = process.env.PORT || 3000;
 
- const passport = require("passport");
- const port = process.env.PORT || 3000;
+  //Initialize Express App for XS UAA and HDBEXT Middleware
+  const app = express();
+  //logging
+  app.use(morgan("dev"));
+  app.use(
+  	bodyParser.json({
+  		limit: "200mb"
+  	})
+  );
+  const logging = require("@sap/logging");
+  const appContext = logging.createAppContext();
 
- //Initialize Express App for XS UAA and HDBEXT Middleware
- const app = express();
- //logging
- app.use(morgan("dev"));
- app.use(
- 	bodyParser.json({
- 		limit: "200mb"
- 	})
- );
- const logging = require("@sap/logging");
- const appContext = logging.createAppContext();
+  const helmet = require("helmet");
+  // ...
+  app.use(helmet());
+  app.use(helmet.contentSecurityPolicy({
+  	directives: {
+  		defaultSrc: ["'self'"],
+  		styleSrc: ["'self'", "sapui5.hana.ondemand.com"],
+  		scriptSrc: ["'self'", "sapui5.hana.ondemand.com"]
+  	}
+  }));
+  // Sets "Referrer-Policy: no-referrer".
+  app.use(helmet.referrerPolicy({
+  	policy: "no-referrer"
+  }));
 
- const helmet = require("helmet");
- // ...
- app.use(helmet());
- app.use(helmet.contentSecurityPolicy({
- 	directives: {
- 		defaultSrc: ["'self'"],
- 		styleSrc: ["'self'", "sapui5.hana.ondemand.com"],
- 		scriptSrc: ["'self'", "sapui5.hana.ondemand.com"]
- 	}
- }));
- // Sets "Referrer-Policy: no-referrer".
- app.use(helmet.referrerPolicy({
- 	policy: "no-referrer"
- }));
+  // passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+  // 	uaa: {
+  // 		tag: "xsuaa"
+  // 	}
+  // }).uaa));
 
- // passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
- // 	uaa: {
- // 		tag: "xsuaa"
- // 	}
- // }).uaa));
+  app.use(logging.middleware({
+  	appContext: appContext,
+  	logNetwork: true
+  }));
+  // app.use(passport.initialize());
+  var hanaOptions = xsenv.getServices({
+  	hana: {
+  		tag: "hana"
+  	}
+  });
+  hanaOptions.hana.pooling = true;
+  app.use(
+  	// 	passport.authenticate("JWT", {
+  	// 		session: false
+  	// 	}),
+  	xsHDBConn.middleware(hanaOptions.hana)
+  );
 
- app.use(logging.middleware({
- 	appContext: appContext,
- 	logNetwork: true
- }));
- // app.use(passport.initialize());
- var hanaOptions = xsenv.getServices({
- 	hana: {
- 		tag: "hana"
- 	}
- });
- hanaOptions.hana.pooling = true;
- app.use(
- 	// 	passport.authenticate("JWT", {
- 	// 		session: false
- 	// 	}),
- 	xsHDBConn.middleware(hanaOptions.hana)
- );
+  //Compression
+  app.use(require("compression")({
+  	threshold: "1b"
+  }));
+  // Handling cors
+  const cors = require("cors");
+  app.use(cors());
+  // compress responses
+  app.use(compression());
+  app.use(function (req, res, next) {
+  	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  	res.header(
+  		"Access-Control-Allow-Headers",
+  		"Origin, X-Requested-With, Content-Type, Accept"
+  	);
+  	next();
+  });
 
- //Compression
- app.use(require("compression")({
- 	threshold: "1b"
- }));
- // Handling cors
- const cors = require("cors");
- app.use(cors());
- // compress responses
- app.use(compression());
- app.use(function (req, res, next) {
- 	res.header("Access-Control-Allow-Origin", "*");
- 	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
- 	res.header(
- 		"Access-Control-Allow-Headers",
- 		"Origin, X-Requested-With, Content-Type, Accept"
- 	);
- 	next();
- });
+  // app.get("/", async(req, res) => {
+  // 	try {
+  // 		const dbClass = require("sap-hdbext-promisfied")
+  // 		let db = new dbClass(req.db);
+  // 		const statement = await db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
+  // 		const results = await db.statementExecPromisified(statement, [])
+  // 		let result = JSON.stringify({
+  // 			Objects: results
+  // 		})
+  // 		return res.type("application/json").status(200).send(result)
+  // 	} catch (e) {
+  // 		return res.type("text/plain").status(500).send(`ERROR: ${e.toString()}`)
+  // 	}
+  // });
+  // //  TESTING STUFF 
 
- // app.get("/", async(req, res) => {
- // 	try {
- // 		const dbClass = require("sap-hdbext-promisfied")
- // 		let db = new dbClass(req.db);
- // 		const statement = await db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
- // 		const results = await db.statementExecPromisified(statement, [])
- // 		let result = JSON.stringify({
- // 			Objects: results
- // 		})
- // 		return res.type("application/json").status(200).send(result)
- // 	} catch (e) {
- // 		return res.type("text/plain").status(500).send(`ERROR: ${e.toString()}`)
- // 	}
- // });
- // //  TESTING STUFF 
+  // app.get("/env", (req, res) => {
+  // 	return res.type("application/json").status(200).send(JSON.stringify(process.env));
+  // });
 
- // app.get("/env", (req, res) => {
- // 	return res.type("application/json").status(200).send(JSON.stringify(process.env));
- // });
+  // ROUTES
 
- // ROUTES
+  // login and signup
+  const adminauthRoutes = require("./router/auth");
+  const userauthRoutes = require("./router/auth/userindex.js");
+  app.use("/admin", adminauthRoutes);
+  app.use("/user", userauthRoutes);
 
- // news, events, faq routes
- const adminnefRoutes = require("./router/nef");
- const usernefRoutes = require("./router/nef");
- app.use("/admin/action", adminnefRoutes);
- app.use("/user/actions", usernefRoutes);
+  // news, events, faq routes
+  const adminnefRoutes = require("./router/nef");
+  const usernefRoutes = require("./router/nef");
+  app.use("/admin/action", adminnefRoutes);
+  app.use("/user/action", usernefRoutes);
 
- // documents 
- const admindocumentRoutes = require("./router/documents");
- const userdocumentRoutes = require("./router/documents/userindex.js");
- app.use("/admin/action", admindocumentRoutes);
- app.use("/user/actions", userdocumentRoutes);
+  // documents 
+  const admindocumentRoutes = require("./router/documents");
+  const userdocumentRoutes = require("./router/documents/userindex.js");
+  app.use("/admin/action", admindocumentRoutes);
+  app.use("/user/action", userdocumentRoutes);
 
- // user 
- // admin
- // login and signup
- const adminauthRoutes = require("./router/auth");
- const userauthRoutes = require("./router/auth/userindex.js");
- app.user("/admin", adminauthRoutes);
- app.use("/user", userauthRoutes);
+  // admin
+  const adminactionRoutes = require("./router/admin");
+  app.use("/admin/action", adminactionRoutes);
 
- // additional serivce
- // 1. usersearch, 
- // ASKHR
+  // user
+  const adminuseractionRoutes = require("./routes/users");
+  const useractionRoutes = require("./routes/users/userindex.js");
+  app.use("/admin/action", adminuseractionRoutes);
+  app.use("/admin/action", useractionRoutes);
 
- app.listen(port, () => {
- 	console.log(`Server listening on port: ${port}`);
- });
+  // ADDITIONAL SERVICES 
+
+  // usersearch
+  const searchRoutes = require("./routes/search");
+  app.use("/admin/action", searchRoutes);
+  app.use("/admin/action", searchRoutes);
+
+  // ASKHR
+  //NEED TO BE BUILD AS A SAPERATE PRODUCT.
+
+  app.listen(port, () => {
+  	console.log(`Server listening on port: ${port}`);
+  });
