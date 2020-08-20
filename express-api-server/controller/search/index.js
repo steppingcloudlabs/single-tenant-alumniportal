5dbd4edf4a4c266b8179b610d6a5522860457807
prakritidev <prakritidev.4088@gmail.com>
2020-08-20 09:55:51 +0000

    -  feat: added search engine controller; updated router

---
+++ express-api-server/controller/search/index.js
@@ -0,0 +1,111 @@
+/* eslint-disable */
+const searchSerivce = require("../../service/search")();
+const dbClass = require("sap-hdbext-promisfied");
+module.exports = {
+	user: async(req, res) => {
+		try {
+			const payload = req.query;
+			let db = new dbClass(req.db);
+			const response = await searchService.searchUser({
+				payload,
+				db
+			});
+			if (response) {
+				res.status(200).send({
+					status: "200",
+					result: response,
+				});
+			} else {
+				res.status(400).send({
+					status: "400",
+					result: response
+				});
+			}
+		} catch (error) {
+			res.status(400).send({
+				status: "400",
+				result: error
+			});
+		}
+	},
+	skill: async(req, res) => {
+		try {
+			const payload = req.query;
+			let db = new dbClass(req.db);
+			const response = await searchService.searchSkill({
+				payload,
+				db
+			});
+			if (response) {
+				res.status(200).send({
+					status: "200",
+					result: response,
+				});
+			} else {
+				res.status(400).send({
+					status: "400",
+					result: response
+				});
+			}
+		} catch (error) {
+			res.status(400).send({
+				status: "400",
+				result: error
+			});
+		}
+
+	},
+	admin: async(req, res) => {
+		try {
+			const payload = req.query;
+			let db = new dbClass(req.db);
+			const response = await searchService.searchAdmin({
+				payload,
+				db
+			});
+			if (response) {
+				res.status(200).send({
+					status: "200",
+					result: response,
+				});
+			} else {
+				res.status(400).send({
+					status: "400",
+					result: response
+				});
+			}
+		} catch (error) {
+			res.status(400).send({
+				status: "400",
+				result: error
+			});
+		}
+
+	},
+	job: async(req, res) => {
+		try {
+			const payload = req.query;
+			let db = new dbClass(req.db);
+			const response = await searchService.searchJob({
+				payload,
+				db
+			});
+			if (response) {
+				res.status(200).send({
+					status: "200",
+					result: response,
+				});
+			} else {
+				res.status(400).send({
+					status: "400",
+					result: response
+				});
+			}
+		} catch (error) {
+			res.status(400).send({
+				status: "400",
+				result: error
+			});
+		}
+	}
+}
