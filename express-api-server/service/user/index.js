1c76018217eab5295773cd12316d2b77cdb11918
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-25 13:25:51 +0000

    feat: get user profile

--- express-api-server/service/user/index.js
+++ express-api-server/service/user/index.js
@@ -1,25 +1,17 @@
+const uuid = require("uuid");
+const currentSchema = require("../../utils/database/index.js")();
 module.exports = () => {
-	/*
-	SERVICE FUNCTIONS FOR NEWS 
-	
-	REPONSIBILITY: HAMZA
-	*/
 	const getprofile = ({
 		payload,
 		db
 	}) => {
 		return new Promise(async(resolve, reject) => {
 			try {
-				console.log("hello")
-				
-				// const schema = currentSchema(db)
-				// 	// TODO: add pagination using [to, from] clauses in statement.
-				// const limit = payload.limit == undefined ? 10 : payload.limit
-				// const offset = payload.offset == undefined ? 0 : payload.offset
-				// const statement = await db.preparePromisified(
-				// 	`SELECT "ID","TITLE","CONTENT","AUTHOR","TAGS","DATE","PHOTO","PHOTONAME","CREATEDAT","CREATEDBY","MODIFIEDAT","MODIFIEDBY" FROM "${schema}"."SCLABS_ALUMNIPORTAL_NEWS_NEWS" rows limit ${limit} offset ${offset}`
-				// )
-				 const results = await db.statementExecPromisified(statement, [])
+				const schema = currentSchema(db)
+				const statement = await db.preparePromisified(
+					`SELECT FROM "${schema}"."SCLABS_ALUMNIPORTAL_MASTERDATA_MASTERDATA"  WHERE ID = '${payload.id}'`
+				)
+				const results = await db.statementExecPromisified(statement, [])
 				resolve(results);
 
 			} catch (error) {
@@ -27,9 +19,10 @@ module.exports = () => {
 			}
 		});
 	};
-		return {
-		getprofile,
-	
+
+	return {
+		getprofile
 
 	};
-}
+
+};
