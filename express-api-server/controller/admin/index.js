b07fbc60a5a30a7c43d275fee912153b89da3fa6
sana1207 <sanafatema97@gmail.com>
2020-08-27 09:36:15 +0000

    -feat: created delete API

--- express-api-server/controller/admin/index.js
+++ express-api-server/controller/admin/index.js
@@ -2,34 +2,6 @@ const adminserivce = require("../../service/admin")();
 const dbClass = require("sap-hdbext-promisfied");
 
     module.exports = {
-    	getuser: async(req, res) => {
-			try {
-				const payload = req.params;
-				let db = new dbClass(req.db);
-				const response = await adminserivce.viewuser({
-					payload,
-					db
-				});
-				if (response) {
-					res.status(200).send({
-						status: "200",
-						result: response,
-					});
-				} else {
-					res.status(400).send({
-						status: "400",
-						result: response
-					});
-				}
-
-			} catch (error) {
-				res.status(400).send({
-					status: "400",
-					result: error
-				});
-			}
-
-		},
 		createuser: async(req, res) => {
 			try {
 				const payload = req.body;
@@ -61,37 +33,6 @@ const dbClass = require("sap-hdbext-promisfied");
 			}
 
 		},
-		
-		deleteuser: async(req, res) => {
-			try {
-
-				const payload = req.body;
-				let db = new dbClass(req.db);
-
-				const response = await adminserivce.deleteuser({
-					payload,
-					db
-				});
-
-				if (response) {
-					res.type("application/json").status(200).send({
-						status: "200",
-						result: response
-					});
-				} else {
-					res.type("text/plain").status(200).send({
-						status: "500",
-						result: "Error"
-					});
-
-				}
-			} catch (error) {
-				res.status(200).send({
-					status: "400",
-					result: "Element Not Found"
-				});
-			}
-		},
 		updateuser: async(req, res) => {
 			try {
 				const payload = req.body;
@@ -120,6 +61,64 @@ const dbClass = require("sap-hdbext-promisfied");
 				});
 			}
         },
+        getuser: async(req, res) => {
+    	
+			try {
+			
+				const payload = req.params;
+				let db = new dbClass(req.db);
+				const response = await adminserivce.getuser({
+					payload,
+					db
+				});
+			
+				if (response) {
+					res.type("application/json").status(200).send({
+						status: "200",
+						result: response,
+					});
+				} else {
+					res.status(400).send({
+						status: "400",
+						result: response
+					});
+				}
+
+			} catch (error) {
+				res.status(400).send({
+					status: "400",
+					result: error
+				});
+			}
+
+		},
+		deleteuser: async(req, res) => {
+			try {
+				const payload = req.body;
+				let db = new dbClass(req.db);
+				const response = await adminserivce.deleteuser({
+					payload,
+					db
+				});
+				if (response) {
+					res.type("application/json").status(200).send({
+						status: "200",
+						result: response
+					});
+				} else {
+					res.type("text/plain").status(200).send({
+						status: "500",
+						result: "Error"
+					});
+
+				}
+			} catch (error) {
+				res.status(200).send({
+					status: "400",
+					result: "Element Not Found"
+				});
+			}
+		},
 		
     }
    
