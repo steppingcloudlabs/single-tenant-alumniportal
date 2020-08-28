84070865d56d1fe752a37000a5abb04c61e0ca95
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-28 12:35:09 +0000

    - feat: get user profile complete

--- express-api-server/service/user/index.js
+++ express-api-server/service/user/index.js
@@ -1,5 +1,6 @@
 const uuid = require("uuid");
-const currentSchema = require("../../utils/database/index.js")();
+const utils = require("../../utils/database/index.js")();
+// const currentSchema = require("../../utils/database/index.js")();
 module.exports = () => {
 	const getprofile = ({
 		payload,
@@ -7,9 +8,9 @@ module.exports = () => {
 	}) => {
 		return new Promise(async(resolve, reject) => {
 			try {
-				const schema = currentSchema(db)
+				const schema = await utils.currentSchema({db})
 				const statement = await db.preparePromisified(
-					`SELECT FROM "${schema}"."SCLABS_ALUMNIPORTAL_MASTERDATA_MASTERDATA"  WHERE ID = '${payload.id}'`
+					`SELECT * FROM "${schema}"."SCLABS_ALUMNIPORTAL_MASTERDATA_MASTERDATA"  WHERE ID = '${payload.id}'`
 				)
 				const results = await db.statementExecPromisified(statement, [])
 				resolve(results);
