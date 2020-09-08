f77e2704f0cfceed2600d7cb0c3a855233706b3b
sana1207 <sanafatema97@gmail.com>
2020-09-08 09:30:13 +0000

    -feat: Created "createbulkjob"

--- express-api-server/service/job/index.js
+++ express-api-server/service/job/index.js
@@ -8,7 +8,9 @@ module.exports = () => {
 	}) => {
 		return new Promise(async(resolve, reject) => {
 			try {
-				const schema = await utils.currentSchema({db})
+				const schema = await utils.currentSchema({
+						db
+					})
 					// TODO: add pagination using [to, from] clauses in statement.
 				const limit = payload.limit == undefined ? 10 : payload.limit
 				const offset = payload.offset == undefined ? 0 : payload.offset
@@ -30,7 +32,9 @@ module.exports = () => {
 	}) => {
 		return new Promise(async(resolve, reject) => {
 			try {
-				const schema = await utils.currentSchema({db});
+				const schema = await utils.currentSchema({
+					db
+				});
 				const createdat = new Date().toISOString();
 				const createdby = "admin";
 				const modifiedby = "admin";
@@ -41,12 +45,13 @@ module.exports = () => {
 				const posting_end_date = payload.payload.posting_end_date;
 				const posting_start_date = payload.payload.posting_start_date;
 				const job_posting_status = payload.payload.job_posting_status;
-				const title =payload.payload.title;
+				const title = payload.payload.title;
 				const job_role = payload.payload.job_role;
-				const job_details =payload.payload.job_details;
-				const country_of_residence =payload.payload.country_of_residence;
+				const job_details = payload.payload.job_details;
+				const country_of_residence = payload.payload.country_of_residence;
 				const city = payload.payload.city;
-			    const query = `INSERT INTO "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB" VALUES(	
+				const query =
+					`INSERT INTO "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB" VALUES(	
 				        '${createdat}',
 						'${createdby}',
 						'${modifiedat}',
@@ -61,31 +66,34 @@ module.exports = () => {
 					    '${job_details}',
 						'${country_of_residence}',
 						'${city}')`
-			    const statement = await db.preparePromisified(query);
-		        const results = await db.statementExecPromisified(statement, [])
-                resolve(results);
+				const statement = await db.preparePromisified(query);
+				const results = await db.statementExecPromisified(statement, [])
+				resolve(results);
 			} catch (error) {
 				reject(error);
 			}
 		});
 	};
 
-	const updatejob= ({
+	const updatejob = ({
 		payload,
 		db
 	}) => {
 		return new Promise(async(resolve, reject) => {
 			try {
-			
-				const schema = await utils.currentSchema({db});
+
+				const schema = await utils.currentSchema({
+					db
+				});
 				const createdat = new Date().toISOString();
 				const createdby = "admin";
 				const modifiedby = "admin";
 				const modifiedat = new Date().toISOString();
 				const date = new Date().toISOString();
 				const id = uuid();
-				const job_details =payload.payload.job_details;
-				const query =`UPDATE "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB"
+				const job_details = payload.payload.job_details;
+				const query =
+					`UPDATE "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB"
 					SET "JOB_DETAILS" = CASE 
 					WHEN '${payload.payload.job_details}' != 'undefined' THEN '${payload.payload.job_details}'
 					ELSE (select "JOB_DETAILS" FROM "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB" where "ID"='${payload.payload.id}')
@@ -94,7 +102,7 @@ module.exports = () => {
     				"MODIFIEDAT" = '${modifiedat}'
     				where
     				"ID" = '${payload.payload.id}'`
-   
+
 				const statement = await db.preparePromisified(query)
 				const results = await db.statementExecPromisified(statement, [])
 				resolve(results)
@@ -114,7 +122,9 @@ module.exports = () => {
 				/*console.log(
 					`DELETE FROM "${schema}"."SCLABS_ALUMNIPORTAL_NEWS_NEWS"  WHERE ID = '${payload.id}'`
 				)*/
-				const schema = await utils.currentSchema({db})
+				const schema = await utils.currentSchema({
+					db
+				})
 				const query = `DELETE FROM "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB"  WHERE ID = '${payload.payload.id}'`
 				const statement = await db.preparePromisified(query);
 				const results = await db.statementExecPromisified(statement, [])
@@ -125,11 +135,74 @@ module.exports = () => {
 		});
 	};
 
+	const createbulkjob = ({
+		payload,
+		db
+	}) => {
+		return new Promise(async(resolve, reject) => {
+			try {
+				res = [];
+
+				// console.log(payload.payload)
+				for (var i = 0; i < payload.payload.length; i++) {
+					// var obj = new Object();
+					// obj.payload = payload.payload[i];
+					// var jsonString = JSON.stringify(obj);
+					// console.log(jsonString);
+					const schema = await utils.currentSchema({
+						db
+					});
+					const createdat = new Date().toISOString();
+					const createdby = "admin";
+					const modifiedby = "admin";
+					const modifiedat = new Date().toISOString();
+					const date = new Date().toISOString();
+					const id = uuid();
+					console.log(payload);
+					const requisition_id = payload.payload[i].requisition_id;
+					const posting_end_date = payload.payload[i].posting_end_date;
+					const posting_start_date = payload.payload[i].posting_start_date;
+					const job_posting_status = payload.payload[i].job_posting_status;
+					const title = payload.payload[i].title;
+					const job_role = payload.payload[i].job_role;
+					const job_details = payload.payload[i].job_details;
+					const country_of_residence = payload.payload[i].country_of_residence;
+					const city = payload.payload[i].city;
+					const query = `INSERT INTO "${schema}"."SCLABS_ALUMNIPORTAL_JOB_JOB" VALUES(
+						             '${createdat}',
+						             '${createdby}',
+						             '${modifiedat}',
+						             '${modifiedby}',
+						             '${id}',
+						             '${requisition_id}',
+						             '${posting_end_date}',
+						             '${posting_start_date}',
+						             '${job_posting_status}',
+						             '${title}',
+						             '${job_role}',
+						             '${job_details}',
+						             '${country_of_residence}',
+						             '${city}')`
+					const statement = await db.preparePromisified(query);
+					const result = await db.statementExecPromisified(statement, [])
+					console.log(result);
+					res.push(result);
+				}
+				resolve(res);
+			} catch (error) {
+				console.log(error);
+				reject(error);
+
+			}
+		});
+	};
+
 	return {
 		createjob,
 		updatejob,
 		getjob,
 		deletejob,
+		createbulkjob
 	};
 
 };
