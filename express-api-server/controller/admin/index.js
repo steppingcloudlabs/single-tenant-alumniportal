39e38c5a98c644e26c9ff75823d0724cca80c121
prakritidev <prakritidev.4088@gmail.com>
2020-09-10 10:19:09 +0000

    feat: reviewed and changes responses. (delete user is not working)

--- express-api-server/controller/admin/index.js
+++ express-api-server/controller/admin/index.js
@@ -1,124 +1,123 @@
 const adminserivce = require("../../service/admin")();
 const dbClass = require("sap-hdbext-promisfied");
 
-    module.exports = {
-		createuser: async(req, res) => {
-			try {
-				const payload = req.body;
-			
-				let db = new dbClass(req.db);
-				const response = await adminserivce.createuser({
-					payload,
-					db
-				});
-				console.log(response);
-				if (response) {
-					res.type("application/json").status(200).send({
-						status: "200",
-						result: response
-					});
-				} else {
-					res.type("text/plain").status(200).send({
-						status: "500",
-						result: "Error"
-				
-					});
+module.exports = {
+	createuser: async(req, res) => {
+		try {
+			const payload = req.body;
 
-				}
-			} catch (error) {
-				res.type("text/plain").status(500).send({
-					status: "500",
-					error: error
+			let db = new dbClass(req.db);
+			const response = await adminserivce.createuser({
+				payload,
+				db
+			});
+			console.log(response);
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response
 				});
+			} else {
+				res.type("application/json").status(200).send({
+					status: "500",
+					result: "Error"
+
+				});
+
+			}
+		} catch (error) {
+			res.type("application/json").status(500).send({
+				status: "500",
+				error: error
+			});
+		}
+
+	},
+	updateuser: async(req, res) => {
+		try {
+			const payload = req.body;
+			let db = new dbClass(req.db);
+			const response = await adminserivce.updateuser({
+				payload,
+				db
+			});
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response
+				});
+			} else {
+				res.type("application/json").status(200).send({
+					status: "500",
+					result: "Error"
+				});
+
 			}
 
-		},
-		updateuser: async(req, res) => {
-			try {
-				const payload = req.body;
-				let db = new dbClass(req.db);
-				const response = await adminserivce.updateuser({
-					payload,
-					db
-				});
-				if (response) {
-					res.type("application/json").status(200).send({
-						status: "200",
-						result: response
-					});
-				} else {
-					res.type("text/plain").status(200).send({
-						status: "500",
-						result: "Error"
-					});
+		} catch (error) {
+			res.type("application/json").status(500).send({
+				status: "500",
+				error: error
+			});
+		}
+	},
+	getuser: async(req, res) => {
 
-				}
+		try {
 
-			} catch (error) {
-				res.type("text/plain").status(500).send({
-					status: "500",
-					error: error
-				});
-			}
-        },
-        getuser: async(req, res) => {
-    	
-			try {
-			
-				const payload = req.params;
-				let db = new dbClass(req.db);
-				const response = await adminserivce.getuser({
-					payload,
-					db
-				});
-			
-				if (response) {
-					res.type("application/json").status(200).send({
-						status: "200",
-						result: response,
-					});
-				} else {
-					res.status(400).send({
-						status: "400",
-						result: response
-					});
-				}
+			const payload = req.params;
+			let db = new dbClass(req.db);
+			const response = await adminserivce.getuser({
+				payload,
+				db
+			});
 
-			} catch (error) {
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response,
+				});
+			} else {
 				res.status(400).send({
 					status: "400",
-					result: error
+					result: response
 				});
 			}
 
-		},
-		deleteuser: async(req, res) => {
-			try {
-				const payload = req.body;
-				let db = new dbClass(req.db);
-				const response = await adminserivce.deleteuser({
-					payload,
-					db
-				});
-				if (response) {
-					res.type("application/json").status(200).send({
-						status: "200",
-						result: response
-					});
-				} else {
-					res.type("text/plain").status(200).send({
-						status: "500",
-						result: "Error"
-					});
+		} catch (error) {
+			res.status(400).send({
+				status: "400",
+				result: error
+			});
+		}
 
-				}
-			} catch (error) {
-				res.status(200).send({
-					status: "400",
-					result: "Element Not Found"
+	},
+	deleteuser: async(req, res) => {
+		try {
+			const payload = req.body;
+			let db = new dbClass(req.db);
+			const response = await adminserivce.deleteuser({
+				payload,
+				db
+			});
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response
 				});
+			} else {
+				res.type("application/json").status(200).send({
+					status: "404",
+					result: response
+				});
+
 			}
-		},
-		
-    }
-   
+		} catch (error) {
+			res.status(200).send({
+				status: "400",
+				result: "Element Not Found"
+			});
+		}
+	},
+
+}
