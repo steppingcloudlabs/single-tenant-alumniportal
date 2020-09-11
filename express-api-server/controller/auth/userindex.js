1b25d15b76ba45ab5bf3800ec1f467a6b2faa4ca
maaz1996 <maaz.abdullah111@gmail.com>
2020-09-11 12:21:25 +0000

    added joi validator for login; forget password and reset password function added

--- express-api-server/controller/auth/userindex.js
+++ express-api-server/controller/auth/userindex.js
@@ -95,6 +95,38 @@ const dbClass = require("sap-hdbext-promisfied");
 				});
 			}
         },
+        forgetpassword: async(req, res) => {
+			try {
+				const payload = req.body;
+				let db = new dbClass(req.db);
+				const response = await authserivce.forgetpassword({
+					payload,
+					db
+				});
+				console.log(response)
+			
+				if(response) {
+				 
+					res.type("application/json").status(200).send({
+						status: "200",
+						result: response
+					});
+				}
+				 else {
+					res.type("text/plain").status(200).send({
+						status: "500",
+						result: "Error"
+					});
+
+				 }
+
+			} catch (error) {
+				res.type("text/plain").status(500).send({
+					status: "500",
+					error: error
+				});
+			}
+        },
         
 		
     }
--- express-api-server/router/auth/userindex.js
+++ express-api-server/router/auth/userindex.js
@@ -2,12 +2,20 @@ const expressrouter = require("express").Router();
 const authcontroller = require("../../controller/auth/userindex.js");
 const {
   signupValidateBody,
-  signupSchemas}=require("../../validator/authValidate.js")
+  signupSchemas,signinValidateBody,signinSchemas}=require("../../validator/authValidate.js")
 //auth routers
 expressrouter
 	.route("/login")
-	.post((req, res, next) => authcontroller.login(req, res, next));
+	.post(signinValidateBody(signinSchemas.authSchema),(req, res, next) => authcontroller.login(req, res, next));
 expressrouter
 	.route("/signup")
 	.post(signupValidateBody(signupSchemas.authSchema),(req, res, next) => authcontroller.signup(req, res, next));
+	expressrouter
+	.route("/forgetpassword")
+	.post((req, res, next) => authcontroller.forgetpassword(req, res, next));
+	expressrouter
+	.route("/reset/:token")
+	.post((req, res, next) => authcontroller.resetpassword(req, res, next));
+	
+	
 module.exports = expressrouter;
