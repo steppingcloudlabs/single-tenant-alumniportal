7f539598760e7deb14a6a08e33fad8a756cf5519
sana1207 <sanafatema97@gmail.com>
2020-09-25 06:06:44 +0000

    -feat: Created dev-alumniportal_0.0.1.mtar file

--- express-api-server/server.js
+++ express-api-server/server.js
@@ -10,15 +10,33 @@
   const JWTtoken = require("./middleware/JWTtoken/tokenchecks")()
   const passport = require("passport");
   const port = process.env.PORT || 3000;
-
+  const lib = require('./library');
+  const hdbext = require('@sap/hdbext');
+  const cfenv = require('cfenv');
+  const appEnv = cfenv.getAppEnv();
+  const services = xsenv.getServices({
+  	uaa: {
+  		tag: 'xsuaa'
+  	},
+  	registry: {
+  		tag: 'SaaS'
+  	},
+  	sm: {
+  		label: 'service-manager'
+  	},
+  	dest: {
+  		tag: 'destination'
+  	}
+  });
   //Initialize Express App for XS UAA and HDBEXT Middleware
   const app = express();
+  app.use(bodyParser.json());
   //logging
   app.use(morgan("dev"));
   app.use(
-    bodyParser.json({
-      limit: "200mb"
-    })
+  	bodyParser.json({
+  		limit: "200mb"
+  	})
   );
 
   const logging = require("@sap/logging");
@@ -28,45 +46,45 @@
   // ...
   app.use(helmet());
   app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'", "sapui5.hana.ondemand.com"],
-      scriptSrc: ["'self'", "sapui5.hana.ondemand.com"]
-    }
+  	directives: {
+  		defaultSrc: ["'self'"],
+  		styleSrc: ["'self'", "sapui5.hana.ondemand.com"],
+  		scriptSrc: ["'self'", "sapui5.hana.ondemand.com"]
+  	}
   }));
   // Sets "Referrer-Policy: no-referrer".
   app.use(helmet.referrerPolicy({
-    policy: "no-referrer"
+  	policy: "no-referrer"
   }));
 
   passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
-    uaa: {
-      tag: "xsuaa"
-    }
+  	uaa: {
+  		tag: "xsuaa"
+  	}
   }).uaa));
   app.use(passport.initialize());
 
   app.use(logging.middleware({
-    appContext: appContext,
-    logNetwork: true
+  	appContext: appContext,
+  	logNetwork: true
   }));
 
   var hanaOptions = xsenv.getServices({
-    hana: {
-      tag: "hana"
-    }
+  	hana: {
+  		tag: "hana"
+  	}
   });
   hanaOptions.hana.pooling = true;
   app.use(
-    passport.authenticate("JWT", {
-      session: false
-    }),
-    xsHDBConn.middleware(hanaOptions.hana)
+  	passport.authenticate("JWT", {
+  		session: false
+  	}),
+  	xsHDBConn.middleware(hanaOptions.hana)
   );
 
   //Compression
   app.use(require("compression")({
-    threshold: "1b"
+  	threshold: "1b"
   }));
   // Handling cors
   const cors = require("cors");
@@ -74,59 +92,186 @@
   // compress responses
   app.use(compression());
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  	res.header(
+  		"Access-Control-Allow-Headers",
+  		"Origin, X-Requested-With, Content-Type, Accept"
+  	);
+  	next();
   });
+
   //multi-tenant SaaS registry endpoints 
-  app.get("/tenantinfo", function (req, res) {
-    var responseStr = "<!DOCTYPE HTML><html><head><title>MTApp</title></head><body><h1>MTApp</h1><h2>Welcome " + req.authInfo.userInfo.givenName +
-      " " + req.authInfo.userInfo.familyName + "!</h2><p><b>Subdomain:</b> " + req.authInfo.subdomain + "</p><p><b>Identity Zone:</b> " + req.authInfo
-      .identityZone + "</p></body></html>";
-    res.status(200).send(responseStr);
+  
+  //app.get("/", async (req, res) => {
+  //  try {
+  //    const dbClass = require("sap-hdbext-promisfied")
+  //    let db = new dbClass(req.db);
+  //    const statement = await db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
+  //    const results = await db.statementExecPromisified(statement, [])
+  //    let result = JSON.stringify({
+  //      Objects: results
+  //    })
+  //    return res.type("application/json").status(200).send(result)
+  //  } catch (e) {
+  //    return res.type("text/plain").status(500).send(`ERROR: ${e.toString()}`)
+  //  }
+  //});
+  app.put('/callback/v1.0/tenants/*', function (req, res) {
+  	let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry
+  		.appName.toLowerCase().replace(/_/g, '-');
+  	let tenantURL = 'https:\/\/' + tenantHost + /\.(.*)/gm.exec(appEnv.app.application_uris[0])[0];
+  	console.log('Subscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost, tenantURL);
+  	lib.createRoute(tenantHost, services.registry.appName).then(
+  		function (result) {
+  			lib.createSMInstance(services.sm, services.registry.appName + '-' + req.body.subscribedTenantId).then(
+  				async function (result) {
+  					res.status(200).send(tenantURL);
+  				},
+  				function (err) {
+  					console.log(err.stack);
+  					res.status(500).send(err.message);
+  				});
+  		},
+  		function (err) {
+  			console.log(err.stack);
+  			res.status(500).send(err.message);
+  		});
   });
-
-  // subscribe/onboard a subscriber tenant
-  app.put("/callback/v1.0/tenants/*", function (req, res) {
-    var tenantAppURL = "https:\/\/" + req.body.subscribedSubdomain + "-dev-alumniportal-ui" + ".cfapps.eu10.hana.ondemand.com";
-    res.status(200).send(tenantAppURL);
-  });
-
   // unsubscribe/offboard a subscriber tenant
-  app.delete("/callback/v1.0/tenants/*", function (req, res) {
-    res.status(200).send("");
+  app.delete('/callback/v1.0/tenants/*', function (req, res) {
+  	let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry
+  		.appName.toLowerCase().replace(/_/g, '-');
+  	console.log('Unsubscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost);
+  	lib.deleteRoute(tenantHost, services.registry.appName).then(
+  		function (result) {
+  			lib.deleteSMInstance(services.sm, services.registry.appName + '-' + req.body.subscribedTenantId).then(
+  				function (result) {
+  					res.status(200).send('');
+  				},
+  				function (err) {
+  					console.log(err.stack);
+  					res.status(500).send(err.message);
+  				});
+  		},
+  		function (err) {
+  			console.log(err.stack);
+  			res.status(500).send(err.message);
+  		});
   });
-  // server env variables.
-  app.get("/env", (req, res) => {
-    return res.type("application/json").status(200).send(JSON.stringify(process.env));
-  });
-  app.get("/", async (req, res) => {
-    try {
-      const dbClass = require("sap-hdbext-promisfied")
-      let db = new dbClass(req.db);
-      const statement = await db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
-      const results = await db.statementExecPromisified(statement, [])
-      let result = JSON.stringify({
-        Objects: results
-      })
-      return res.type("application/json").status(200).send(result)
-    } catch (e) {
-      return res.type("text/plain").status(500).send(`ERROR: ${e.toString()}`)
-    }
+  // get reuse service dependencies
+  app.get('/callback/v1.0/dependencies', function (req, res) {
+  	let tenantId = req.params.tenantId;
+  	let dependencies = [{
+  		'xsappname': services.dest.xsappname
+  	}];
+  	console.log('Dependencies: ', tenantId, dependencies);
+  	res.status(200).json(dependencies);
   });
 
+  // app user info
+  app.get('/srv/info', function (req, res) {
+  	if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+  		let info = {
+  			'userInfo': req.authInfo.userInfo,
+  			'subdomain': req.authInfo.subdomain,
+  			'tenantId': req.authInfo.identityZone
+  		};
+  		res.status(200).json(info);
+  	} else {
+  		res.status(403).send('Forbidden');
+  	}
+  });
+
+  // app subscriptions
+  app.get('/srv/subscriptions', function (req, res) {
+  	if (req.authInfo.checkScope('$XSAPPNAME.Administrator')) {
+  		lib.getSubscriptions(services.registry).then(
+  			function (result) {
+  				res.status(200).json(result);
+  			},
+  			function (err) {
+  				console.log(err.stack);
+  				res.status(500).send(err.message);
+  			});
+  	} else {
+  		res.status(403).send('Forbidden');
+  	}
+  });
+
+  // app database
+  app.get('/srv/database', function (req, res) {
+  	if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+  		// get DB instance
+  		lib.getSMInstance(services.sm, services.registry.appName + '-' + req.authInfo.identityZone).then(
+  			function (serviceBinding) {
+  				if (!serviceBinding.hasOwnProperty('error')) {
+  					// connect to DB instance
+  					let hanaOptions = serviceBinding.credentials;
+  					hdbext.createConnection(hanaOptions, function (err, db) {
+  						if (err) {
+  							console.log(err.message);
+  							res.status(500).send(err.message);
+  							return;
+  						}
+  						// insert
+  						let sqlstmt = `INSERT INTO "test.db::tenantInfo" ("tenant", "timeStamp") VALUES('` + services.registry.appName + `-` + req.authInfo
+  							.subdomain + `-` + req.authInfo.identityZone + `', CURRENT_TIMESTAMP)`;
+  						db.exec(sqlstmt, function (err, results) {
+  							if (err) {
+  								console.log(err.message);
+  								res.status(500).send(err.message);
+  								return;
+  							}
+  							// query
+  							sqlstmt = 'SELECT * FROM "test.db::tenantInfo"';
+  							db.exec(sqlstmt, function (err, results) {
+  								if (err) {
+  									console.log(err.message);
+  									res.status(500).send(err.message);
+  									return;
+  								}
+  								res.status(200).json(results);
+  							});
+  						});
+  					});
+  				} else {
+  					res.status(500).send(serviceBinding);
+  				}
+  			},
+  			function (err) {
+  				console.log(err.stack);
+  				res.status(500).send(err.message);
+  			});
+  	} else {
+  		res.status(403).send('Forbidden');
+  	}
+  });
+
+  // destination reuse service
+  app.get('/srv/destinations', function (req, res) {
+  	if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+  		lib.getDestination(services.dest, req.authInfo.subdomain, req.query.destination).then(
+  			function (result) {
+  				// result contains the destination information for use in REST calls
+  				res.status(200).json(result);
+  			},
+  			function (err) {
+  				console.log(err.stack);
+  				res.status(500).send(err.message);
+  			});
+  	} else {
+  		res.status(403).send('Forbidden');
+  	}
+  });
 
   //user authorization routes
   const userauthRoutes = require("./router/auth/userindex.js");
   app.use("/user/auth", userauthRoutes);
   //tokenization: tokens check middleware
   app.use(JWTtoken)
-  // ROUTES
-  //skills
+  	// ROUTES
+  	//skills
   const adminskillsRoutes = require("./router/skills");
   const userskillsRoutes = require("./router/skills/userindex.js");
   app.use("/admin/action", adminskillsRoutes);
@@ -161,7 +306,6 @@
 
   // TODO Maazzzz
 
-
   // TODO PD
   // ADDITIONAL SERVICES 
 
@@ -174,6 +318,6 @@
   //skills
 
   app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
+  	console.log(`Server listening on port: ${port}`);
   });
   module.exports = express;
