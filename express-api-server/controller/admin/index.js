0742ca3e2cc98f869a2c269ea6cb5b7ae4e34fd7
prakritidev <prakritidev.4088@gmail.com>
2020-10-14 12:07:37 +0000

    feat: added controllers for admin actions.

--- express-api-server/controller/admin/index.js
+++ express-api-server/controller/admin/index.js
@@ -5,27 +5,25 @@ module.exports = {
 	createuser: async(req, res) => {
 		try {
 			const payload = req.body;
-		
+
 			let db = new dbClass(req.db);
 			const response = await adminserivce.createuser({
 				payload,
 				db
 			});
-		
-					if (response=="userexists") {
+
+			if (response == "userexists") {
 				res.type("application/json").status(200).send({
 					status: "200",
 					result: "User Id already exists"
 				});
-			}
-			else {
+			} else {
 				res.type("application/json").status(200).send({
 					status: "200",
 					result: response
 				});
 			}
-		
-		
+
 		} catch (error) {
 			res.type("application/json").status(500).send({
 				status: "500",
@@ -65,15 +63,14 @@ module.exports = {
 	getuser: async(req, res) => {
 		try {
 			const payload = req.params;
-		
-			
+
 			let db = new dbClass(req.db);
 			const response = await adminserivce.getuser({
 				payload,
-			
+
 				db
 			});
-		
+
 			if (response) {
 				res.type("application/json").status(200).send({
 					status: "200",
@@ -98,6 +95,121 @@ module.exports = {
 			const payload = req.body;
 			let db = new dbClass(req.db);
 			const response = await adminserivce.deleteuser({
+				payload,
+				db
+			});
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response
+				});
+			} else {
+				res.type("application/json").status(200).send({
+					status: "404",
+					result: response
+				});
+
+			}
+		} catch (error) {
+			res.status(200).send({
+				status: "400",
+				result: "Element Not Found"
+			});
+		}
+	},
+	createadmin: async(req, res) => {
+		try {
+			const payload = req.body;
+
+			let db = new dbClass(req.db);
+			const response = await adminserivce.createadmin({
+				payload,
+				db
+			});
+
+			if (response == "userexists") {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: "User Id already exists"
+				});
+			} else {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response
+				});
+			}
+
+		} catch (error) {
+			res.type("application/json").status(500).send({
+				status: "500",
+				error: error
+			});
+		}
+
+	},
+	updateadmin: async(req, res) => {
+		try {
+			const payload = req.body;
+			let db = new dbClass(req.db);
+			const response = await adminserivce.updateadmin({
+				payload,
+				db
+			});
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response
+				});
+			} else {
+				res.type("application/json").status(200).send({
+					status: "500",
+					result: "Error"
+				});
+
+			}
+
+		} catch (error) {
+			res.type("application/json").status(500).send({
+				status: "500",
+				error: error
+			});
+		}
+	},
+	getadmin: async(req, res) => {
+		try {
+			const payload = req.params;
+
+			let db = new dbClass(req.db);
+			const response = await adminserivce.getadmin({
+				payload,
+
+				db
+			});
+
+			if (response) {
+				res.type("application/json").status(200).send({
+					status: "200",
+					result: response,
+				});
+			} else {
+				res.status(400).send({
+					status: "400",
+					result: response
+				});
+			}
+		} catch (error) {
+			res.status(400).send({
+				status: "400",
+				result: error
+			});
+		}
+
+	},
+	deleteadmin: async(req, res) => {
+		try {
+			const payload = req.body;
+			let db = new dbClass(req.db);
+			const response = await adminserivce.deleteadmin({
 				payload,
 				db
 			});
