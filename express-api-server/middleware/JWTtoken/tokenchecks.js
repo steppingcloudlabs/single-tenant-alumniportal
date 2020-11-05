d714d22f7fcb706a4f1243de71d67227aa342c0d
prakritidev <prakritidev.4088@gmail.com>
2020-11-05 10:18:27 +0000

    feat: removed admincheck

--- express-api-server/middleware/JWTtoken/tokenchecks.js
+++ express-api-server/middleware/JWTtoken/tokenchecks.js
@@ -10,7 +10,7 @@ module.exports = () => {
 		} = req.body;
 		if (token) {
 			const tokendetails = JWT.verify(token, JWT_SECRET);
-			const expirytimefromtoken = tokendetails.exp
+			const expirytimefromtoken = tokendetails.exp;
 			if (Date.now() > expirytimefromtoken) {
 				res.type("application/json").status(200).send({
 					status: "200",
@@ -26,35 +26,6 @@ module.exports = () => {
 			});
 		}
 	}
+	return tokenchecks;
 
-	function admintokenchecks(req, res, next) {
-		const {
-			manager
-		} = req.query;
-		if (manager) {
-			const schema = await utils.currentSchema({
-				db
-			})
-			query = `select `
-			const statement = await db.preparePromisified(query)
-			const results = await db.statementExecPromisified(statement, [])
-			if (results.length == 0) {
-				res.type("application/json").status(200).send({
-					status: "401",
-					result: "Manager is not registered in the system"
-				});
-			} else {
-				next();
-			}
-		} else {
-			res.type("application/json").status(200).send({
-				status: "400",
-				result: "Rejected Request, Manager Required"
-			});
-		}
-	}
-	return {
-		tokenchecks,
-		admintokenchecks
-	};
 };
