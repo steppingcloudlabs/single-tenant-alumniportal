8bdb15f138e12601b59db14052c2cf27e30707eb
prakritidev vema <prakritidev.4088@gmail.com>
2020-12-21 16:15:27 +0530

    feat: added user document status for user api

--- express-api-server/router/documents/userindex.js
+++ express-api-server/router/documents/userindex.js
@@ -4,4 +4,7 @@ const documentcontroller = require("../../controller/nef/index");
 expressrouter
 	.route("/document/get")
 	.get((req, res, next) => documentcontroller.getdocument(req, res, next));
+expressrouter
+	.route("/documents/status")
+	.get((req, res, next) => documentcontroller.statusdocuments(req, res, next));
 module.exports = expressrouter;
