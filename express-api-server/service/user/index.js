7523d1b766329ee20050aa099d99675d1a308763
prakritidev vema <prakritidev.4088@gmail.com>
2020-12-23 02:11:37 +0530

    fix: updated userprofile update api; inserting skills is pending

--- express-api-server/service/user/index.js
+++ express-api-server/service/user/index.js
@@ -28,15 +28,15 @@ module.exports = () => {
 					A1."SALUTATION_PERSONAL_INFORMATION",
 					IFNULL(A1.CITY_ADDRESSES,'') "CITY_ADDRESSES",
 					IFNULL(A1.PHONE_NUMBER_PHONE_INFORMATION, '') "PHONE_NUMBER_PHONE_INFORMATION",
-					IFNULL(A1.MANAGER_JOB_INFORMATION, '') "A1.MANAGER_JOB_INFORMATION",
-					IFNULL(A1.DESIGNATION_JOB_INFORMATION, '') "A1.DESIGNATION_JOB_INFORMATION",
+					IFNULL(A1.MANAGER_JOB_INFORMATION, '') "MANAGER_JOB_INFORMATION",
+					IFNULL(A1.DESIGNATION_JOB_INFORMATION, '') "DESIGNATION_JOB_INFORMATION",
 					IFNULL(A1.SKILL_ID, '') "SKILL_ID", 
 					IFNULL(A1.LINKEDIN, '') "LINKEDIN",
 					IFNULL(A1.USERTYPE, '') "USERTYPE", 
 					IFNULL(A1.PROFILEIMAGE, '') "PROFILEIMAGE",
 					IFNULL(A1.STATE, '') "STATE", 
 					IFNULL(A1.COUNTRY, '') "COUNTRY",
-					A2."SKILL" as skill
+					IFNULL(A2.SKILL, '') as skill
 					FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" as A1 
 					LEFT JOIN  "${schema}"."SCLABS_ALUMNIPORTAL_SKILLS_SKILLS" as A2 
 					ON A1."SKILL_ID" = A2."ID" where A1."USER_ID" = '${userid}';`
@@ -79,33 +79,45 @@ module.exports = () => {
 				const modifiedby = "admin";
 				const modifiedat = new Date().toISOString();
 				const date = new Date().toISOString();
-				logger.info(payload)
-				console.info(payload)
-				const query =
+				let query =
 					`UPDATE "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS"
+
                     SET "PHONE_NUMBER_PHONE_INFORMATION" = CASE
 								WHEN '${payload.payload.PHONE_NUMBER_PHONE_INFORMATION}' != 'undefined' THEN '${payload.payload.PHONE_NUMBER_PHONE_INFORMATION}'
-								ELSE (select "PHONE_NUMBER_PHONE_INFORMATION" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "ID"='${payload.payload.USERID}')
+								ELSE (select "PHONE_NUMBER_PHONE_INFORMATION" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID"='${payload.payload.USERID}')
 								END,
-						"CITY_ADDRESSES" = case
+						"CITY_ADDRESSES" = CASE
 								WHEN '${payload.payload.CITY_ADDRESSES}' != 'undefined' THEN '${payload.payload.CITY_ADDRESSES}'
-								ELSE (select "CITY_ADDRESSES" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "ID"='${payload.payload.USERID}')
+								ELSE (select "CITY_ADDRESSES" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID"='${payload.payload.USERID}')
 								END,
-						"SKILL_ID" = case
-								WHEN '${payload.payload.SKILL}' != 'undefined' THEN '${payload.payload.SKILL}'
-								ELSE (select "SKILL_ID" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "ID"='${payload.payload.USERID}')
+						"PERSONAL_EMAIL_ID" = CASE
+								WHEN '${payload.payload.PERSONAL_EMAIL_ID}' != 'undefined' THEN '${payload.payload.PERSONAL_EMAIL_ID}'
+								ELSE (select "PERSONAL_EMAIL_ID" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID"='${payload.payload.USERID}')
 								END,
-						"LINKEDIN" = case
+						"PROFILEIMAGE" = CASE
+								WHEN '${payload.payload.PROFILEIMAGE}' != 'undefined' THEN '${payload.payload.PROFILEIMAGE}'
+								ELSE (select "PROFILEIMAGE" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID"='${payload.payload.USERID}')
+								END,
+						"STATE" = CASE
+								WHEN '${payload.payload.STATE}' != 'undefined' THEN '${payload.payload.STATE}'
+								ELSE (select "STATE" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID"='${payload.payload.USERID}')
+								END,
+						"COUNTRY" = CASE
+								WHEN '${payload.payload.COUNTRY}' != 'undefined' THEN '${payload.payload.COUNTRY}'
+								ELSE (select "COUNTRY" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID"='${payload.payload.USERID}')
+								END,
+						"LINKEDIN" = CASE
 								WHEN '${payload.payload.LINKEDIN}' != 'undefined' THEN '${payload.payload.LINKEDIN}'
-								ELSE(select "LINKEDIN" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "ID" = '${payload.payload.USERID}')
+								ELSE(select "LINKEDIN" FROM "${schema}"."SCLABS_ALUMNIPORTAL_USERS_USERS" where "USER_ID" = '${payload.payload.USERID}')
 								END,
 					    "MODIFIEDBY" = '${modifiedby}',
 					    "MODIFIEDAT" = '${modifiedat}'
 					where
 					"USER_ID" = '${payload.payload.USERID}'`
 				console.log(query);
-				const statement = await db.preparePromisified(query)
-				const results = await db.statementExecPromisified(statement, [])
+				let statement = await db.preparePromisified(query)
+				let results = await db.statementExecPromisified(statement, [])
+
 				console.log(results)
 				if (results >= 1) {
 					resolve(1);
